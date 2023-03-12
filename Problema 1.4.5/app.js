491cc627c108523dad8fc410e5833b64132e5625
vlad-dragos-iustin-30125 <100029234+vlad-dragos-iustin-30125@users.noreply.github.com>
2023-03-12 12:47:51 +0200

    Update p5

--- Problema 1.4.5/app.js
+++ Problema 1.4.5/app.js
@@ -1,34 +1,36 @@
-$("#add").click(function () {
-  var firstNumber = parseInt($("#firstNumber").val());
-  var secondNumber = parseInt($("#secondNumber").val());
-  var result = firstNumber + secondNumber;
-  $("#result").text(result);
-});
+$(document).ready(function () {
+  $("#add").click(function () {
+    var firstNumber = parseInt($("#firstNumber").val());
+    var secondNumber = parseInt($("#secondNumber").val());
+    var result = firstNumber + secondNumber;
+    $("#result").html(result);
+  });
 
-$("#subtract").click(function () {
-  var firstNumber = parseInt($("#firstNumber").val());
-  var secondNumber = parseInt($("#secondNumber").val());
-  var result = firstNumber - secondNumber;
-  $("#result").text(result);
-});
+  $("#subtract").click(function () {
+    var firstNumber = parseInt($("#firstNumber").val());
+    var secondNumber = parseInt($("#secondNumber").val());
+    var result = firstNumber - secondNumber;
+    $("#result").html(result);
+  });
 
-$("#multiply").click(function () {
-  var firstNumber = parseInt($("#firstNumber").val());
-  var secondNumber = parseInt($("#secondNumber").val());
-  var result = firstNumber * secondNumber;
-  $("#result").text(result);
-});
+  $("#multiply").click(function () {
+    var firstNumber = parseInt($("#firstNumber").val());
+    var secondNumber = parseInt($("#secondNumber").val());
+    var result = firstNumber * secondNumber;
+    $("#result").html(result);
+  });
 
-$("#divide").click(function () {
-  var firstNumber = parseInt($("#firstNumber").val());
-  var secondNumber = parseInt($("#secondNumber").val());
-  var result = firstNumber / secondNumber;
-  $("#result").text(result);
-});
+  $("#divide").click(function () {
+    var firstNumber = parseInt($("#firstNumber").val());
+    var secondNumber = parseInt($("#secondNumber").val());
+    var result = firstNumber / secondNumber;
+    $("#result").html(result);
+  });
 
-$("#modulo").click(function () {
-  var firstNumber = parseInt($("#firstNumber").val());
-  var secondNumber = parseInt($("#secondNumber").val());
-  var result = firstNumber % secondNumber;
-  $("#result").text(result);
+  $("#modulus").click(function () {
+    var firstNumber = parseInt($("#firstNumber").val());
+    var secondNumber = parseInt($("#secondNumber").val());
+    var result = firstNumber % secondNumber;
+    $("#result").html(result);
+  });
 });
